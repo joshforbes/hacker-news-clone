99aac80159ba42731c0b2a9c0cdef3d6257d4dd3
Josh Forbes <jdforbes82@gmail.com>
2019-05-24 23:09:05 -0400

    Fetch actual comments from hacker news

--- src/apis/HackerNews.js
+++ src/apis/HackerNews.js
@@ -2,7 +2,7 @@ const api = `https://hacker-news.firebaseio.com/v0`;
 const json = ".json?print=pretty";
 
 const HackerNews = {
-  async post(id) {
+  async fetch(id) {
     const res = await fetch(`${api}/item/${id}${json}`);
     return await res.json();
   }
--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -2,27 +2,6 @@ import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import HackerNews from "../apis/HackerNews";
 
-const HackerNewsApi = {
-  comments: [
-    {
-      id: 123,
-      parent: 1234,
-      by: "author",
-      text: "a test comment",
-      time: "1558422372",
-      type: "comment"
-    },
-    {
-      id: 124,
-      parent: 12345,
-      by: "another author",
-      text: "some follow up",
-      time: "1558422375",
-      type: "comment"
-    }
-  ]
-};
-
 function PostComments({
   match: {
     params: { id }
@@ -32,16 +11,26 @@ function PostComments({
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setPost(await HackerNews.post(id));
+    const fetchPost = async () => {
+      setPost(await HackerNews.fetch(id));
     };
 
-    fetchData();
+    fetchPost();
   }, [id]);
 
   useEffect(() => {
-    setComments(HackerNewsApi.comments);
-  }, [id]);
+    if (post) {
+      const fetchComments = async () => {
+        const comments = post.kids.map(async id => {
+          return await HackerNews.fetch(id);
+        });
+
+        setComments(await Promise.all(comments));
+      };
+
+      fetchComments();
+    }
+  }, [post]);
 
   if (!post) {
     return <div>Loading...</div>;
@@ -55,10 +44,10 @@ function PostComments({
           return (
             <li key={comment.id}>
               <div>
-                <span>by {post.by}</span>
-                <span>on {post.time}</span>
+                <span>by {comment.by}</span>
+                <span>on {comment.time}</span>
               </div>
-              <p>{comment.text}</p>
+              <p dangerouslySetInnerHTML={{ __html: comment.text }} />
             </li>
           );
         })}
